a41ff3ca6d8767c2e7d0ebbab6592dff5adb6e3d
XanatosX <simonaberle@gmx.de>
2017-06-24 01:18:37 +0200

    Removed debug lines

--- ModularToolManger/ModularToolManger/Program.cs
+++ ModularToolManger/ModularToolManger/Program.cs
@@ -16,11 +16,11 @@ namespace ModularToolManger
         [STAThread]
         static void Main()
         {
-            Settings s = new Settings();
-            s.AddNewField("ToolManager");
-            s.AddKeyValue("ToolManager", "Test", "Hey");
-            s.AddKeyValue("ToolManager", "Test2", false);
-            s.Save();
+            //Settings s = new Settings();
+            //s.AddNewField("ToolManager");
+            //s.AddKeyValue("ToolManager", "Test", "Hey");
+            //s.AddKeyValue("ToolManager", "Test2", false);
+            //s.Save();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             CentralLanguage.Initalize();
