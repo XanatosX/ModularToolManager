960916d5696f98993dcc834983bb12ae36cd8d28
XanatosX <simonaberle@gmx.de>
2018-02-10 12:33:01 +0100

    Refactored central language class Added todo to CentralLogging class

--- ModularToolManger/ModularToolManger/Core/CentralLogging.cs
+++ ModularToolManger/ModularToolManger/Core/CentralLogging.cs
@@ -7,6 +7,7 @@ using System.Text;
 
 namespace ModularToolManger.Core
 {
+    //@TODO find a non static way of doing this ...
     public static class CentralLogging
     {
         public static FileLogger AppDebugLogger = new FileLogger(Path.GetTempPath(), "ModularToolManager_Debug.log");//new BasicLogger(Path.GetTempPath(), "ModularToolManager_Debug.log");
