21448fd2f0fe8f841d3383cd80d7c7125ff463b2
XanatosX <simonaberle@gmx.de>
2018-08-30 22:22:59 +0200

    Fixed naming error Adding new project with basic unit tests

---
+++ ModularToolManger/ModularToolManagerTest/PasswordCryptTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ModularToolManger.Core;
+
+namespace ModularToolManagerTest
+{
+    [TestClass]
+    public class PasswordCryptTest
+    {
+        private static PasswordManager manager;
+
+        [ClassInitialize()]
+        public static void ClassInit(TestContext context)
+        {
+            Console.WriteLine("ClassInit " + context.TestName);
+            manager = new PasswordManager();
+        }
+
+        [TestMethod()]
+        public void SetupTest()
+        {
+            Assert.AreEqual(typeof(PasswordManager), manager.GetType());
+        }
+
+        [TestMethod()]
+        public void EncryptDecryptPasswordTest()
+        {
+            string password = "SomeStrongPassword";
+            string initialData = "Some text to encrypt";
+            string cryptData = manager.EncryptPassword(initialData, password);
+            string afterConvert = manager.DecryptPassword(cryptData, password);
+
+            Assert.AreEqual(initialData, afterConvert);
+        }
+    }
+}
