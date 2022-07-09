b3ac455c7ed2da08e04101118c8669f51fc1744a
XanatosX <10531466+XanatosX@users.noreply.github.com>
2022-07-09 16:35:24 +0200

    Add missing summary blocks

--- src/ModularToolManager2/Services/Language/ILanguageService.cs
+++ src/ModularToolManager2/Services/Language/ILanguageService.cs
@@ -1,18 +1,43 @@
-﻿using System;
+﻿using ModularToolManager2.Models;
 using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ModularToolManager2.Services.Language
+namespace ModularToolManager2.Services.Language;
+
+/// <summary>
+/// Interface to describe the language service
+/// </summary>
+public interface ILanguageService
 {
-    public interface ILanguageService
-    {
-        List<CultureInfo> GetAvailableCultures();
+    /// <summary>
+    /// Get a list with all the available cultures
+    /// </summary>
+    /// <returns></returns>
+    List<CultureInfo> GetAvailableCultures();
 
-        void ChangeLanguage(CultureInfo newCulture);
+    /// <summary>
+    /// Change the language for the service
+    /// </summary>
+    /// <param name="newCulture">The new culture to use</param>
+    void ChangeLanguage(CultureInfo newCulture);
 
-        bool ValidLanguage(CultureInfo culture);
-    }
+    /// <summary>
+    /// Change the language for the service
+    /// </summary>
+    /// <param name="newCulture">The new culture to use</param>
+    void ChangeLanguage(CultureInfoModel newCulture) => ChangeLanguage(newCulture.Culture);
+
+    /// <summary>
+    /// Check if the provided culture is a valid one
+    /// </summary>
+    /// <param name="culture">The culutre to check if it is valid</param>
+    /// <returns>True if the provided culture is valid</returns>
+    bool ValidLanguage(CultureInfo culture);
+
+    /// <summary>
+    /// Check if the provided culture is a valid one
+    /// </summary>
+    /// <param name="culture">The culutre to check if it is valid</param>
+    /// <returns>True if the provided culture is valid</returns>
+    void ValidLanguage(CultureInfoModel newCulture) => ValidLanguage(newCulture.Culture);
 }
--- src/ModularToolManager2/Services/Language/ResourceCultureService.cs
+++ src/ModularToolManager2/Services/Language/ResourceCultureService.cs
@@ -1,54 +1,58 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
+﻿using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ModularToolManager2.Services.Language
+namespace ModularToolManager2.Services.Language;
+
+/// <summary>
+/// Implementation of the language service
+/// </summary>
+internal class ResourceCultureService : ILanguageService
 {
-    internal class ResourceCultureService : ILanguageService
+    /// <summary>
+    /// All the available cultures
+    /// </summary>
+    private List<CultureInfo>? availableCultures;
+
+    /// <inheritdoc/>
+    public void ChangeLanguage(CultureInfo newCulture)
     {
-        private List<CultureInfo>? availableCultures;
-
-        public void ChangeLanguage(CultureInfo newCulture)
+        if (!ValidLanguage(newCulture))
         {
-            if (!ValidLanguage(newCulture))
-            {
-                return;
-            }
-            Properties.Resources.Culture = newCulture;
-            availableCultures = null;
+            return;
         }
+        Properties.Resources.Culture = newCulture;
+        availableCultures = null;
+    }
 
-        public bool ValidLanguage(CultureInfo culture)
-        {
-            return availableCultures.Contains(culture);
-        }
+    /// <inheritdoc/>
+    public bool ValidLanguage(CultureInfo culture)
+    {
+        return availableCultures.Contains(culture);
+    }
 
-        List<CultureInfo> ILanguageService.GetAvailableCultures()
+    /// <inheritdoc/>
+    public List<CultureInfo> GetAvailableCultures()
+    {
+        if (availableCultures != null)
         {
-            if (availableCultures != null)
-            {
-                return availableCultures;
-            }
-            string applicationLocation = Assembly.GetExecutingAssembly().Location;
-            string resoureFileName = Path.GetFileNameWithoutExtension(applicationLocation) + ".resources.dll";
-            DirectoryInfo rootDirectory = new DirectoryInfo(Path.GetDirectoryName(applicationLocation));
-            availableCultures = rootDirectory.GetDirectories()
-                                             .Where(dir => CultureInfo.GetCultures(CultureTypes.AllCultures).Any(culture => culture.Name == dir.Name))
-                                             .Where(dir => File.Exists(Path.Combine(dir.FullName, resoureFileName)))
-                                             .Select(dir => CultureInfo.GetCultureInfo(dir.Name))
-                                             .ToList();
-            if (!availableCultures.Contains(CultureInfo.GetCultureInfo("en")))
-            {
-                availableCultures.Add(CultureInfo.GetCultureInfo("en"));
-            }
-            availableCultures.OrderBy(culture => culture.DisplayName);
             return availableCultures;
         }
+        string applicationLocation = Assembly.GetExecutingAssembly().Location;
+        string resoureFileName = Path.GetFileNameWithoutExtension(applicationLocation) + ".resources.dll";
+        DirectoryInfo rootDirectory = new DirectoryInfo(Path.GetDirectoryName(applicationLocation));
+        availableCultures = rootDirectory.GetDirectories()
+                                         .Where(dir => CultureInfo.GetCultures(CultureTypes.AllCultures).Any(culture => culture.Name == dir.Name))
+                                         .Where(dir => File.Exists(Path.Combine(dir.FullName, resoureFileName)))
+                                         .Select(dir => CultureInfo.GetCultureInfo(dir.Name))
+                                         .ToList();
+        if (!availableCultures.Contains(CultureInfo.GetCultureInfo("en")))
+        {
+            availableCultures.Add(CultureInfo.GetCultureInfo("en"));
+        }
+        availableCultures.OrderBy(culture => culture.DisplayName);
+        return availableCultures;
     }
 }
