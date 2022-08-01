e72a800cf36f4007c94e27dc4bd4710c80c7daf7
XanatosX <10531466+XanatosX@users.noreply.github.com>
2022-08-01 23:09:56 +0200

    Add basic code to start showing functions on main view

--- src/ModularToolManager/ViewModels/FunctionSelectionViewModel.cs
+++ src/ModularToolManager/ViewModels/FunctionSelectionViewModel.cs
@@ -1,8 +1,22 @@
-﻿namespace ModularToolManager.ViewModels;
+﻿using ModularToolManager.Models;
+using ModularToolManager.Services.Functions;
+using Splat;
+using System.Collections.ObjectModel;
+
+namespace ModularToolManager.ViewModels;
 
 /// <summary>
 /// View model to select a function
 /// </summary>
 internal class FunctionSelectionViewModel : ViewModelBase
 {
+    private readonly IFunctionService? functionService;
+
+    public ObservableCollection<FunctionModel> AllFunctions { get; set; }
+
+    public FunctionSelectionViewModel()
+    {
+        functionService = Locator.Current.GetService<IFunctionService>();
+        AllFunctions = new ObservableCollection<FunctionModel>(functionService?.GetAvailableFunctions());
+    }
 }
