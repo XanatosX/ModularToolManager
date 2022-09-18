3ae6adf708183a2d27a8726476291eb2b8e2681e
XanatosX <10531466+XanatosX@users.noreply.github.com>
2022-09-18 21:31:25 +0200

    Add missing summary blocks

--- src/ModularToolManager/Models/ApplicationSettings.cs
+++ src/ModularToolManager/Models/ApplicationSettings.cs
@@ -1,14 +1,27 @@
 ﻿using System.Text.Json.Serialization;
 
 namespace ModularToolManager.Models;
+
+/// <summary>
+/// Class to save application settings
+/// </summary>
 public class ApplicationSettings
 {
+    /// <summary>
+    /// Should the application be always on top of all others
+    /// </summary>
     [JsonPropertyName("always_on_top")]
     public bool AlwaysOnTop { get; set; }
 
+    /// <summary>
+    /// Should the application be started minimized
+    /// </summary>
     [JsonPropertyName("start_minimized")]
     public bool StartMinimized { get; set; }
 
+    /// <summary>
+    /// Show the application in task bar
+    /// </summary>
     [JsonPropertyName("show_in_taskbar")]
     public bool ShowInTaskbar { get; set; }
 }
