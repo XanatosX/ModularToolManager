ab2cc388627805e5166f75a84162a04860609d7c
XanatosX <10531466+XanatosX@users.noreply.github.com>
2022-09-18 21:33:17 +0200

    Add missing summary blocks

--- src/ModularToolManager/ViewModels/SettingsViewModel.cs
+++ src/ModularToolManager/ViewModels/SettingsViewModel.cs
@@ -5,9 +5,6 @@ using CommunityToolkit.Mvvm.Messaging.Messages;
 using ModularToolManager.Models;
 using ModularToolManager.Models.Messages;
 using ModularToolManager.Services.Settings;
-using ModularToolManager.Services.Ui;
-using ModularToolManager.Views;
-
 namespace ModularToolManager.ViewModels;
 
 /// <summary>
@@ -15,17 +12,33 @@ namespace ModularToolManager.ViewModels;
 /// </summary>
 internal partial class SettingsViewModel : ObservableObject
 {
+	/// <summary>
+	/// The settings service to use
+	/// </summary>
 	private readonly ISettingsService settingsService;
 
+	/// <summary>
+	/// Baking field for the top most checkbox
+	/// </summary>
 	[ObservableProperty]
 	private bool topMost;
 
+	/// <summary>
+	/// Baking field if should be started minimitzed
+	/// </summary>
 	[ObservableProperty]
 	private bool startMinimized;
 
+	/// <summary>
+	/// Baking field if should be shown in taskbar
+	/// </summary>
 	[ObservableProperty]
 	private bool showInTaskbar;
 
+	/// <summary>
+	/// Create a new instance of this class
+	/// </summary>
+	/// <param name="settingsService">The settings service to use</param>
 	public SettingsViewModel(ISettingsService settingsService)
 	{
 		this.settingsService = settingsService;
@@ -35,6 +48,9 @@ internal partial class SettingsViewModel : ObservableObject
 		ShowInTaskbar = appSettings.ShowInTaskbar;
 	}
 
+	/// <summary>
+	/// The ok button to save and confirm the changes
+	/// </summary>
 	[RelayCommand]
 	private void Ok()
 	{
@@ -49,6 +65,9 @@ internal partial class SettingsViewModel : ObservableObject
 		}
 	}
 
+	/// <summary>
+	/// The abort button to discard the changes and close the modal
+	/// </summary>
 	[RelayCommand]
 	private void Abort()
 	{
