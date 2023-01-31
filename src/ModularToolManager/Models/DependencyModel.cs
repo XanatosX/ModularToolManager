b730cbfb67837638270d4298775840e0a275a450
XanatosX <10531466+XanatosX@users.noreply.github.com>
2023-01-31 21:00:47 +0100

    Add missing summary blocks

--- src/ModularToolManager/Models/DependencyModel.cs
+++ src/ModularToolManager/Models/DependencyModel.cs
@@ -1,17 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace ModularToolManager.Models;
 
-namespace ModularToolManager.Models;
+/// <summary>
+/// Model for a single dependency
+/// </summary>
 internal class DependencyModel
 {
+    /// <summary>
+    /// The name of the dependency
+    /// </summary>
     public string? Name { get; init; }
 
+    /// <summary>
+    /// The version of the dependency
+    /// </summary>
     public string? Version { get; init; }
 
+    /// <summary>
+    /// The path to the license of the dependency
+    /// </summary>
     public string? LicenseUrl { get; init; }
 
+    /// <summary>
+    /// The path to the project url of the dependency
+    /// </summary>
     public string? ProjectUrl { get; init; }
 }
