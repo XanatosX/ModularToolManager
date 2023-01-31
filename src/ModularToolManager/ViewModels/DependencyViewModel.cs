09a1f394d1c472534a9db087fa94c20a0ebbddd3
XanatosX <10531466+XanatosX@users.noreply.github.com>
2023-01-31 20:54:41 +0100

    Add missing summary blocks
    Prevent null exceptions

--- src/ModularToolManager/ViewModels/DependencyViewModel.cs
+++ src/ModularToolManager/ViewModels/DependencyViewModel.cs
@@ -2,11 +2,17 @@
 using CommunityToolkit.Mvvm.Input;
 using ModularToolManager.Models;
 using ModularToolManagerModel.Services.IO;
-using System.Data;
 
 namespace ModularToolManager.ViewModels;
+
+/// <summary>
+/// View model to show a single dependency
+/// </summary>
 internal partial class DependencyViewModel : ObservableObject
 {
+	/// <summary>
+	/// The dependency to show
+	/// </summary>
 	[ObservableProperty]
 	[NotifyPropertyChangedFor(nameof(Name))]
 	[NotifyPropertyChangedFor(nameof(Version))]
@@ -15,28 +21,59 @@ internal partial class DependencyViewModel : ObservableObject
 	[NotifyPropertyChangedFor(nameof(ProjectSet))]
 	[NotifyPropertyChangedFor(nameof(LicenseSet))]
 	private DependencyModel? dependency;
+
+
+	/// <summary>
+	/// The service used to open an url
+	/// </summary>
 	private readonly IUrlOpenerService urlOpenerService;
 
-	public string? Name => dependency.Name;
+	/// <summary>
+	/// The name of the dependency
+	/// </summary>
+	public string? Name => dependency?.Name;
 
-	public string? Version => dependency.Version;
+	/// <summary>
+	/// The version of the dependency
+	/// </summary>
+	public string? Version => dependency?.Version;
 
-	public string? ProjectUrl => dependency.ProjectUrl;
+	/// <summary>
+	/// The url to the project of the dependency
+	/// </summary>
+	public string? ProjectUrl => dependency?.ProjectUrl;
 
+	/// <summary>
+	/// Is the project url set
+	/// </summary>
 	public bool ProjectSet => !string.IsNullOrEmpty(ProjectUrl);
 
-	public string? LicenseUrl => dependency.LicenseUrl;
+	/// <summary>
+	/// The url to the license of the dependency
+	/// </summary>
+	public string? LicenseUrl => dependency?.LicenseUrl;
 
+	/// <summary>
+	/// is the license for the dependency set
+	/// </summary>
 	public bool LicenseSet => !string.IsNullOrEmpty(LicenseUrl);
 
 
-
+	/// <summary>
+	/// Create a new instance of this class
+	/// </summary>
+	/// <param name="dependency">The depencency to show</param>
+	/// <param name="urlOpenerService">The service used to open the url</param>
 	public DependencyViewModel(DependencyModel dependency, IUrlOpenerService urlOpenerService)
 	{
 		Dependency = dependency;
 		this.urlOpenerService = urlOpenerService;
 	}
 
+	/// <summary>
+	/// Command to open a given url
+	/// </summary>
+	/// <param name="url">The url to open</param>
 	[RelayCommand]
 	public void OpenUrl(string url)
 	{
