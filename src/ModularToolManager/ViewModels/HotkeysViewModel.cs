76286f63867a8b0452d16b0faefd1d83fccd160a
XanatosX <10531466+XanatosX@users.noreply.github.com>
2023-02-01 20:13:44 +0100

    Add missing summary block Small code refactors

--- src/ModularToolManager/ViewModels/HotkeysViewModel.cs
+++ src/ModularToolManager/ViewModels/HotkeysViewModel.cs
@@ -4,11 +4,22 @@ using System.Collections.Generic;
 using System.Linq;
 
 namespace ModularToolManager.ViewModels;
+
+/// <summary>
+/// The view model for all the possible hot keys
+/// </summary>
 internal partial class HotkeysViewModel : ObservableObject
 {
+	/// <summary>
+	/// A list with all the hot keys
+	/// </summary>
 	[ObservableProperty]
 	private List<SingleHotkeyViewModel>? hotkeys;
 
+	/// <summary>
+	/// Create a new instance of this class
+	/// </summary>
+	/// <param name="applicationInformationService">The service used to load the hotkeys</param>
 	public HotkeysViewModel(GetApplicationInformationService applicationInformationService)
 	{
 		Hotkeys = applicationInformationService.GetHotkeys()
