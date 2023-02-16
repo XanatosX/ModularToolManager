2762ef173b4d5dc14600014753b4a6a6aa6d9d0e
XanatosX <10531466+XanatosX@users.noreply.github.com>
2023-02-16 19:34:28 +0100

    Improve setting view Add missing german translations Add missing summary blocks

--- src/ModularToolManager/Models/Messages/ApplicationThemeUpdated.cs
+++ src/ModularToolManager/Models/Messages/ApplicationThemeUpdated.cs
@@ -1,14 +1,16 @@
 ﻿using CommunityToolkit.Mvvm.Messaging.Messages;
-using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ModularToolManager.Models.Messages;
+
+/// <summary>
+/// Mesage that the application theme got changed
+/// </summary>
 internal class ApplicationThemeUpdated : ValueChangedMessage<int>
 {
+    /// <summary>
+    /// Create a new instance of this class
+    /// </summary>
+    /// <param name="value">The theme id the application got updated to</param>
     public ApplicationThemeUpdated(int value) : base(value)
     {
     }
