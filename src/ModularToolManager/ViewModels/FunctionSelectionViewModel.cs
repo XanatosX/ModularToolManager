094f26459aaff9fe69b70c0aad1319ecad279390
XanatosX <10531466+XanatosX@users.noreply.github.com>
2023-07-30 20:41:49 +0200

    Remove import

--- src/ModularToolManager/ViewModels/FunctionSelectionViewModel.cs
+++ src/ModularToolManager/ViewModels/FunctionSelectionViewModel.cs
@@ -10,7 +10,6 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace ModularToolManager.ViewModels;
 
