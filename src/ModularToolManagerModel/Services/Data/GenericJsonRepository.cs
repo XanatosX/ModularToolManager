a4691acfa4169d141f4a74386007039d3c8a964b
XanatosX <10531466+XanatosX@users.noreply.github.com>
2023-08-02 20:19:55 +0200

    Add missing doc blocks
    Fix bug with reseted sort order on edit function
    Always use +10 on sort order for new functions

--- src/ModularToolManagerModel/Services/Data/GenericJsonRepository.cs
+++ src/ModularToolManagerModel/Services/Data/GenericJsonRepository.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.Extensions.Logging;
-using ModularToolManager.Services.Serialization;
+﻿using ModularToolManager.Services.Serialization;
 using ModularToolManagerModel.Services.Data;
 using ModularToolManagerModel.Services.IO;
 
@@ -41,16 +40,27 @@ internal class GenericJsonRepository<T, I> : IRepository<T, I> where T : IReposi
     {
     }
 
+    /// <summary>
+    /// Create a new instance of this repository
+    /// </summary>
+    /// <param name="fileName">The filename to use for the repository</param>
+    /// <param name="fileSystemService">The file system service to use</param>
+    /// <param name="serializeService">The serialize service to use</param>
     public GenericJsonRepository(
         string fileName,
         IFileSystemService fileSystemService,
         ISerializeService serializeService)
     {
-        this.repositoryFilePath = fileName;
+        repositoryFilePath = fileName;
         this.fileSystemService = fileSystemService;
         this.serializeService = serializeService;
     }
 
+    /// <summary>
+    /// Delete a entry from the repository
+    /// </summary>
+    /// <param name="entity">The entity to delete</param>
+    /// <returns>True if the deletion was successful</returns>
     public bool Delete(T entity)
     {
         List<T> entities = GetAll(item => !item.Id?.Equals(entity.Id) ?? true).ToList();
@@ -58,22 +68,41 @@ internal class GenericJsonRepository<T, I> : IRepository<T, I> where T : IReposi
         return FindById(entity.Id) is null;
     }
 
+    /// <summary>
+    /// Delete a entity by it's id
+    /// </summary>
+    /// <param name="id">The id of the entity to delete</param>
+    /// <returns>True if successfully deleted</returns>
     public bool DeleteById(I id)
     {
         var entity = FindById(id);
         return entity is null ? false : Delete(entity);
     }
 
+    /// <summary>
+    /// Find an entity by it's id
+    /// </summary>
+    /// <param name="id">The id of the entity to finx</param>
+    /// <returns>The entity or null if nothing was found</returns>
     public T? FindById(I id)
     {
         return GetAll().FirstOrDefault(item => item.Id?.Equals(id) ?? false);
     }
 
+    /// <summary>
+    /// Get all the entites which are available and do match the given filter
+    /// </summary>
+    /// <param name="filter">The filter to use for searching the entities</param>
+    /// <returns>A list with entities matching the given filter</returns>
     public IEnumerable<T> GetAll(Func<T, bool> filter)
     {
         return GetAll().Where(filter);
     }
 
+    /// <summary>
+    /// Get all the entites which are available
+    /// </summary>
+    /// <returns>A list with all the available entites</returns>
     public IEnumerable<T> GetAll()
     {
         if (!File.Exists(repositoryFilePath))
@@ -93,6 +122,11 @@ internal class GenericJsonRepository<T, I> : IRepository<T, I> where T : IReposi
         return result;
     }
 
+    /// <summary>
+    /// Insert a new entity into the dataset
+    /// </summary>
+    /// <param name="entity">The entity to add</param>
+    /// <returns>The newly inserted entity or null if nothing was added</returns>
     public T? Insert(T entity)
     {
         if (FindById(entity.Id) is not null)
@@ -105,6 +139,11 @@ internal class GenericJsonRepository<T, I> : IRepository<T, I> where T : IReposi
         return SaveData(entities) ? FindById(entity.Id) : default;
     }
 
+    /// <summary>
+    /// Update an entity with the new data
+    /// </summary>
+    /// <param name="entity">The entity to update</param>
+    /// <returns>The newly updated entity or null if something went wrong</returns>
     public T? Update(T entity)
     {
         List<T> entities = GetAll().ToList();
@@ -125,6 +164,11 @@ internal class GenericJsonRepository<T, I> : IRepository<T, I> where T : IReposi
         return SaveData(entities) ? FindById(entity.Id) : default;
     }
 
+    /// <summary>
+    /// Save the data to the disc
+    /// </summary>
+    /// <param name="dataSet">The dataset to store</param>
+    /// <returns>True if saving was successful</returns>
     private bool SaveData(IEnumerable<T> dataSet)
     {
         FileInfo info = new FileInfo(repositoryFilePath);
